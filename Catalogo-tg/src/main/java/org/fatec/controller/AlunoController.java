d6b3496742362834ac7de4c4dcc985370e23b43a
Trabalho <Trabalho@DESKTOP-ADR2II7>
2017-02-16 08:51:36 -0200

    Implementando o modulo de aluno

---
+++ Catalogo-tg/src/main/java/org/fatec/controller/AlunoController.java
@@ -0,0 +1,17 @@
+package org.fatec.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class AlunoController {
+
+	public void cadastrarAluno(){
+		
+	}
+	
+	public void cancelar(){
+		System.out.println("teste");
+	}
+}
